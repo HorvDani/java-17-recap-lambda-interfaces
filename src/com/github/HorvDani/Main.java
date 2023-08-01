4e0c7ffde44bb1621fde21b03dd7e73d3a0e8018
Daniel Horvath <horv.dan94@gmail.com>
2023-08-01 17:02:54 +0200

    feat
    Lambda Expressions, functional interfaces, and method references.

--- src/com/github/HorvDani/Main.java
+++ src/com/github/HorvDani/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class Main {
 
@@ -32,7 +34,7 @@ public class Main {
         //We can use Comparator.comparing instead of lambda
         people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName()));
         System.out.println(people);
-        
+
         interface EnhancedComparator<T> extends Comparator<T> {
             int secondLevel(T o1, T o2);
         }
@@ -53,5 +55,99 @@ public class Main {
         // We created sorting by first names if the people's last names match
         people.sort(comparatorMixed);
         System.out.println(people);
+
+
+        List<String> list = new ArrayList<>(List.of(
+                "alpha", "bravo", "charlie", "delta"));
+
+        for(String s: list){
+            System.out.println(s);
+        }
+
+        System.out.println("--------------");
+
+        list.forEach(myString -> System.out.println(myString));
+
+        System.out.println("--------------");
+
+        String prefix = "nato";
+        list.forEach((var myString) ->{
+            char first = myString.charAt(0);
+            System.out.println(prefix + " " + myString + " means " + first);
+        });
+        //prefix = "NATO";  variable in lambda needs to be final
+
+        Consumer<String> printWords = new Consumer<String>() {
+            @Override
+            public void accept(String sentence) {
+                String[] parts = sentence.split(" ");
+                for(String part : parts){
+                    System.out.println(part);
+                }
+            }
+        };
+        printWords.accept("Let's split this up into an array");
+        System.out.println("--------------");
+
+        Consumer<String> printWordsLambda = sentence -> {
+            String[] parts = sentence.split(" ");
+            for(String part : parts){
+                System.out.println(part);
+            }
+        };
+
+        printWordsLambda.accept("Let's split this up into an array");
+        System.out.println("--------------");
+
+        Consumer<String> printWordsForEach = sentence -> {
+            String[] parts = sentence.split(" ");
+            Arrays.asList(parts).forEach(s -> System.out.println(s));
+        };
+
+        printWordsForEach.accept("Let's split this up into an array");
+        System.out.println("--------------");
+
+        Consumer<String> printWordsConcise = sentence -> {
+            Arrays.asList(sentence.split(" ")).forEach(s -> System.out.println(s));
+        };
+
+        printWordsConcise.accept("Let's split this up into an array");
+
+        System.out.println("--------------");
+
+        int result = calculator((var a,var b) -> a + b, 5, 2);
+        var result2 = calculator((a,b) -> a / b, 10.0, 2.5);
+        var result3 = calculator((a,b) -> a.toUpperCase() + " " + b.toUpperCase(),
+                "Ralph", "Kramden");
+
+        var coords = Arrays.asList(
+                new double[]{47.2160, -95.2348},
+                new double[]{29.1566, -89.2495},
+                new double[]{35.1556, -90.0659});
+
+        coords.forEach(s -> System.out.println(Arrays.toString(s)));
+
+        BiConsumer<Double, Double> p1 = (lat, lng) ->
+                System.out.printf("[lat:%.3f lon:%.3f]%n", lat, lng);
+
+        var firstPoint = coords.get(0);
+        processPoint(firstPoint[0], firstPoint[1], p1);
+
+        System.out.println("--------------");
+
+        coords.forEach(s -> processPoint(s[0], s[1], p1));
+
+        coords.forEach(s -> processPoint(s[0], s[1], (lat, lng) ->
+                System.out.printf("[lat:%.3f lon:%.3f]%n", lat, lng))); // nested lambda
+    }
+    public static <T> T calculator(Operation<T> function, T value1, T value2){
+
+        T result = function.operate(value1, value2);
+        System.out.println("Result of operation: " + result);
+        return result;
+    }
+
+    public static <T> void processPoint(T t1, T t2, BiConsumer<T, T> consumer){
+        consumer.accept(t1, t2);
     }
 }
